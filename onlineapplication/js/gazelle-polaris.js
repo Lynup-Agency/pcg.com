07c5e58301cc195288bbc7199578469905679e0b
jkipker <jkipker@gmail.com>
2014-08-28 09:54:39 -0700

    isaac production updates to form

--- onlineapplication/js/gazelle-polaris.js
+++ onlineapplication/js/gazelle-polaris.js
@@ -7,3 +7,15 @@ function 	setApplicationId()
 	appid.value = (appidVal == null) ? '' : appidVal;
 	//alert(appid.value + ":" + appidVal);
 }
+function 	setApplicationIdForFileUpload()
+{
+	//alert('inside - ' + window.location.href + ';' + window.location.search);
+	var appidVal = window.location.search.split('=')[1];
+	//alert(appidVal);
+	var appid = document.getElementById('applicationId2');
+	appid.value = (appidVal == null) ? '' : appidVal;
+	//alert(appid.value + ":" + appidVal);
+	var uftval = document.getElementById('uploadFileType');
+	var cp = document.getElementById('currentPage');
+	document.forms[0].action = document.forms[0].action + "?applicationId=" + appid.value + "&uploadFileType="+uftval.value + "&currentPage="+cp.value;
+}
